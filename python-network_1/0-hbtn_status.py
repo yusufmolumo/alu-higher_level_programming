a03ff0ccaa720660d4c01970531edfd102a72de8
yusufmolumo <y.molumo@alustudent.com>
2023-11-26 03:26:17 -0800

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Fetches a specified URL and displays the body response."""
+"""Fetches https://alu-intranet.hbtn.io/status."""
 import urllib.request
 
-def fetch_and_display(url):
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
         body = response.read()
@@ -10,12 +11,3 @@ def fetch_and_display(url):
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
-
-if __name__ == "__main__":
-    # Fetch and display status from https://alu-intranet.hbtn.io/status
-    print("Fetching from https://alu-intranet.hbtn.io/status:")
-    fetch_and_display("https://alu-intranet.hbtn.io/status")
-
-    # Fetch and display status from http://0.0.0.0:5050/status
-    print("\nFetching from http://0.0.0.0:5050/status:")
-    fetch_and_display("http://0.0.0.0:5050/status")
