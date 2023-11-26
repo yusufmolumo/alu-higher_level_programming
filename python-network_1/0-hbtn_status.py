4e5556dea14423d5bc39a0e8ff0032a20783b8a4
yusufmolumo <y.molumo@alustudent.com>
2023-11-26 03:13:16 -0800

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-"""
-Fetches both https://alu-intranet.hbtn.io/status and https://intranet.hbtn.io/status
-"""
-
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
-def fetch_and_print(url):
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
-        utf_content = content.decode('utf-8')
-
-        print(f"Body response for {url}:")
-        print("\t- type:", type(content))
-        print("\t- content:", content)
-        print("\t- utf8 content:", utf_content)
 
 if __name__ == "__main__":
-    url1 = 'https://alu-intranet.hbtn.io/status'
-    url2 = 'https://intranet.hbtn.io/status'
-
-    fetch_and_print(url1)
-    fetch_and_print(url2)
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
