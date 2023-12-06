f106dde7a0819c759c609f25a2e1494e118a8c0a
yusufmolumo <y.molumo@alustudent.com>
2023-12-05 23:04:16 -0800

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,33 +1,42 @@
->>> text_indentation = __import__('5-text_indentation').text_indentation
->>> text_indentation("tomato     .   ,,,,/\?00")
-tomato     .
-<BLANKLINE>
-,,,,/\?
-<BLANKLINE>
-00
->>> text_indentation("....")
-.
-<BLANKLINE>
-.
-<BLANKLINE>
-.
-<BLANKLINE>
-.
-<BLANKLINE>
->>> text_indentation("")
+#!/usr/bin/python3
+"""
+prints text in special format
+"""
 
->>> text_indentation(" .? ")
-.
-<BLANKLINE>
-?
-<BLANKLINE>
->>> text_indentation("     .     . ")
-.
-<BLANKLINE>
-.
-<BLANKLINE>
->>> text_indentation("  ")
 
->>> text_indentation(""")
-Traceback (most recent call last):
-SyntaxError: EOF while scanning triple-quoted string literal
+def text_indentation(text):
+    """prints a text with 2 new lines after each of these characters: . ? :
+    Args:
+        text (str): a string
+    Raises:
+        TypeError: if text is not a string
+    """
+    new = ""
+    substring = ""
+    temp = [] * 2
+    flag = 0
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for i in range(len(text)):
+        if i < len(text) and text[i] == '.' or text[i] == '?'\
+           or text[i] == ':':
+            if flag == 0:
+                temp = text.split(text[i], 1)
+                flag = 1
+            else:
+                temp = substring.split(text[i], 1)
+            new += temp[0].lstrip(' ') + text[i]
+            substring = temp[1]
+            print("{:s}".format(new))
+            print()
+            new = ""
+    if flag == 0:
+        text = text.lstrip(' ')
+        text = text.rstrip(' ')
+        print("{:s}".format(text), end="")
+    else:
+        substring = substring.lstrip(' ')
+        substring = substring.rstrip(' ')
+        print("{:s}".format(substring), end="")
